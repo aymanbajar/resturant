210ea805f2a1a54472bd04798aaed8b654497df5
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-12-04 16:44:02 +0300

    sytle(components): add text style in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-orange-500 text-white font-serif py-6 mt-12">
-      <div className="container mx-auto px-4 text-center">
+      <div className="container mx-auto px-4 text-center text-sm md:text-xl lg:text-2xl xl:text-3xl">
         <p>&copy; {new Date().getFullYear()} RecipeShare. All rights reserved.</p>
       </div>
     </footer>
